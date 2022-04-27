3916049955a29c26f5451e01408d3852512332cc
junhyuk <jnhk123@naver.com>
2022-04-27 23:12:42 +0900

    commit

--- src/views/layout/FooterView.vue
+++ src/views/layout/FooterView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer app v-if="$store.state.appFooterVisible">
     <v-card flat tile class="text-center">
-      <v-card-text class="black--text"> made by. 2022 </v-card-text>
+      <v-card-text class="black--text"> made by. devcury </v-card-text>
     </v-card>
   </v-footer>
 </template>
